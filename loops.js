a9afe754a600eddd98d5ec7b914532a66d83bc1b
Kevin O'Reilly <molldokor@gmail.com>
2018-08-07 21:56:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoops(array) {
   for (var i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.");
-    } else {
+    if (i > 1) {
       array.push(`I am ${i} strange loops`);
+    } else if (i === 1) {
+      array.push("I am 1 strange loop.");
     }
   }
   return array;
