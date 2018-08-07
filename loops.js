401f5b271ffc17ca34c2ef85a22d8a4c51086e0f
Kevin O'Reilly <molldokor@gmail.com>
2018-08-07 22:04:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (var i = 0; i < 26; i++) {
+  for (var i = 0; i < 25; i++) {
     if (i === 1) {
       array[i - 1] = "I am 1 strange loop.";
     } else if (i > 1) {
